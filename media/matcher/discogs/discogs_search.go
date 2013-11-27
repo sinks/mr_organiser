24a95a87f2047f1d5350ac122d6917a41a6f1efb
Lincoln Fitzsimons <lincoln.f08@gmail.com>
2013-11-27 23:45:14 +1100

    discogs search continue

--- media/matcher/discogs/discogs_search.go
+++ media/matcher/discogs/discogs_search.go
@@ -37,6 +37,73 @@ type SearchParameters struct {
     Contributer     string      `param:"contributer"`
 }
 
+type Result struct {
+    Pagination      Pagination  `json:"pagination"`
+}
+
+type Pagination struct {
+    PerPage         int         `json:"per_page"`
+    Items           int         `json:"pages"`
+    Page            int         `json:"page"`
+    Urls            PaginationUrls  `json:"urls"`
+    Items           int         `param:"items"`
+}
+
+type PaginationUrls struct {
+    Last            string      `json:"last"`
+    Next            string      `json:"next"`
+}
+
+type LabelResult struct {
+    Id              int         `json:"id"`
+    Uri             string      `json:"uri"`
+    ResourceUrl     string      `json:"resource_url"`
+    Title           string      `json:"title"`
+    Type            string      `json:"type"`
+    Thumb           string      `json:"thumb"`
+}
+
+type MasterResult struct {
+    Id              int         `json:"id"`
+    Style           []string    `json:"style"`
+    Thumb           string      `json:"thumb"`
+    Format          []string    `json:"format"`
+    Country         string      `json:"country"`
+    Title           string      `json:"title"`
+    Uri             string      `json:"uri"`
+    Label           []string    `json:"label"`
+    CatalogNumber   string      `json:"catno"`
+    Year            string      `json:"year"`
+    Genre           []string    `json:"genre"`
+    ResourceUrl     string      `json:"resource_url"`
+    Type            string      `json:"type"`
+}
+
+type ArtistResult struct {
+    Id              int         `json:"id"`
+    Uri             string      `json:"uri"`
+    ResourceUrl     string      `json:"resource_url"`
+    Title           string      `json:"title"`
+    Type            string      `json:"type"`
+    Thumb           string      `json:"thumb"`
+}
+
+type ReleaseResult struct {
+    Id              int         `json:"id"`
+    Style           []string    `json:"style"`
+    Thumb           string      `json:"thumb"`
+    Format          []string    `json:"format"`
+    Country         string      `json:"country"`
+    Title           string      `json:"title"`
+    Uri             string      `json:"uri"`
+    Label           []string    `json:"label"`
+    CatalogNumber   string      `json:"catno"`
+    Year            string      `json:"year"`
+    Genre           []string    `json:"genre"`
+    ResourceUrl     string      `json:"resource_url"`
+    Type            string      `json:"type"`
+}
+
 type DiscogsSearch struct {
     Parameters      SearchParameters
 }
