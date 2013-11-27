d7d7c4462b39e3c5d2afef39859a75c2bce2b10e
Lincoln Fitzsimons <lincoln.f08@gmail.com>
2013-11-27 23:49:03 +1100

    add some urls for discogs

--- media/matcher/discogs/discogs.go
+++ media/matcher/discogs/discogs.go
@@ -1,6 +1,10 @@
 
 package discogs
 
+// Go interface to the discogs api
+// official python client https://github.com/discogs/discogs_client
+// api docs http://www.discogs.com/developers/
+
 var (
     apiUrl = "http://api.discogs.com"
 )
